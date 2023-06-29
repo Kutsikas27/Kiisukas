f44da9d3d71a51c35cfc492b9329a71d9f8f2530
kutsikas27 <mardus80@gmail.com>
2023-06-29 13:59:47 +0300

    playing online since added

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,17 @@
 const { Events, ActivityType } = require('discord.js');
+const { DateTime } = require('luxon');
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
-    client.user.setActivity('Fight Club', {
-      type: ActivityType.Watching,
+    const uptime = DateTime.fromJSDate(client.readyAt)
+      .setZone('Europe/Tallinn')
+      .toFormat('dd.MM HH:mm');
+
+    console.log(uptime);
+    client.user.setActivity(`online since ${uptime}`, {
+      type: ActivityType.Playing,
     });
   },
 };
