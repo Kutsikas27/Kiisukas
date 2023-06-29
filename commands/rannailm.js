2f0c79614d3b5ecfdf43d367d92b85b01fb4579b
kutsikas27 <mardus80@gmail.com>
2023-06-29 16:11:17 +0300

    rannailm error handling

--- commands/rannailm.js
+++ commands/rannailm.js
@@ -1,6 +1,27 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { request } = require('undici');
 const { DateTime } = require('luxon');
+const choices = [
+  'Pirita rand',
+  'Stroomi rand',
+  'Kakumäe rand',
+  'Harku järv',
+  'Pikakari rand',
+  'Paralepa rand',
+  'Vasikaholmi rand',
+  'Pärnu rand',
+  'Emajõgi',
+  'Anne kanal',
+  'Verevi järv',
+  'Viljandi järv',
+  'Viljandi Paala järv',
+  'Tõrva Riiska rand',
+  'Tõrva Vanamõisa rand',
+  'Kuressaare rand',
+  'Paide tehisjärv',
+  'Põlva paisjärv',
+  'Pühajärv',
+].map((e) => ({ name: e, value: e }));
 
 const flagMap = {
   yellow: '<:yellow:1101471020860313640>',
@@ -10,9 +31,10 @@ const flagMap = {
   Default: '<:grey:1105458610126987355>',
 };
 const getOneBeachDescription = (searchString, beaches) => {
-  const rand = beaches.find((el) => el.name === searchString);
+  const beach = beaches.find((el) => el.name === searchString);
+  if (!beach) return null;
 
-  return getBeachRow(rand);
+  return getBeachRow(beach);
 };
 
 const getBeachListDescription = (beaches) => {
@@ -47,29 +69,7 @@ module.exports = {
             .setName('rand')
             .setDescription('sisesta ranna nimi')
             .setRequired(true)
-            .addChoices(
-              { name: 'Pirita rand', value: 'Pirita rand' },
-              {
-                name: 'Pelgurand / Stroomi rand',
-                value: 'Pelgurand / Stroomi rand',
-              },
-              { name: 'Kakumäe rand', value: 'Kakumäe rand' },
-              { name: 'Harku järv', value: 'Harku järv' },
-              { name: 'Pikakari rand', value: 'Pikakari rand' },
-              { name: 'Paralepa rand', value: 'Paralepa rand' },
-              { name: 'Vasikaholmi rand', value: 'Vasikaholmi rand' },
-              { name: 'Pärnu rand', value: 'Pärnu rand' },
-              { name: 'Emajõe vabaujula', value: 'Emajõe vabaujula' },
-              { name: 'Emajõe linnaujula', value: 'Emajõe linnaujula' },
-              { name: 'Anne kanal', value: 'Anne kanal' },
-              { name: 'Verevi järv', value: 'Verevi järv' },
-              { name: 'Viljandi järv', value: 'Viljandi järv' },
-              { name: 'Tõrva Riiska rand', value: 'Tõrva Riiska rand' },
-              { name: 'Tõrva Vanamõisa rand', value: 'Tõrva Vanamõisa rand' },
-              { name: 'Kuressaare rand', value: 'Kuressaare rand' },
-              { name: 'Pühajärv', value: 'Pühajärv' },
-              { name: 'Põlva paisjärv', value: 'Põlva paisjärv' },
-            ),
+            .addChoices(...choices),
         ),
     )
     .addSubcommand((subcommand) =>
@@ -78,11 +78,20 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.deferReply();
-    const result = await request(
-      'https://services.postimees.ee/weather/v4/groups/beach/forecast?type=beach&language=et',
-    );
 
-    const beaches = await result.body.json();
+    const beaches = await request(
+      'https://services.postimees.ee/weather/v4/groups/beach/forecast?type=beach&language=et',
+    )
+      .then((result) => result.body.json())
+      .catch((error) => {
+        console.error('rannailm', error);
+        return null;
+      });
+
+    if (!beaches) {
+      interaction.followUp('tekkis viga');
+      return;
+    }
 
     const subCommand = interaction.options.getSubcommand();
     const searchString = interaction.options.getString('rand');
@@ -90,7 +99,9 @@ module.exports = {
       subCommand === 'rand'
         ? getOneBeachDescription(searchString, beaches)
         : getBeachListDescription(beaches);
-
+    if (!description) {
+      return interaction.followUp('Randa ei leitud');
+    }
     const embed = new EmbedBuilder()
       .setColor(0x1abc9c)
       .setDescription(description);
